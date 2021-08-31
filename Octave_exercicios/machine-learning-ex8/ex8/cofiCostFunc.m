2585329f43a9dc47cbc4627c5cf41bf970560b6e
Gabriel Medina Braga <56964034+medina325@users.noreply.github.com>
2021-08-30 21:23:46 -0400

    Adicionando função de custo e gradientes vetorizados

--- Octave_exercicios/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Octave_exercicios/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -26,37 +26,41 @@ Theta_grad = zeros(size(Theta));
 %               that the gradient is correct. Finally, you should implement
 %               regularization.
 %
-% Notes: X - num_movies  x num_features matrix of movie features
-%        Theta - num_users  x num_features matrix of user features
-%        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
-%            i-th movie was rated by the j-th user
+% Notes: X     - (num_movies x num_features) matrix of movie features
+%        Theta - (num_users x num_features) matrix of user features
+%        Y     - (num_movies x num_users) matrix of user ratings of movies
+%        R     - (num_movies x num_users) matrix, where R(i, j) = 1 if the 
+%                i-th movie was rated by the j-th user
 %
 % You should set the following variables correctly:
 %
-%        X_grad - num_movies x num_features matrix, containing the 
-%                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
+%        X_grad     - (num_movies x num_features) matrix, containing the 
+%                     partial derivatives w.r.t. to each element of X
+%        Theta_grad - (num_users x num_features) matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+% Explanation
+% The matrix R has every row representing a movie and every column a user.
+% Every row of the matrix Theta represents a user with n feature weights,
+% and every row of the matrix X represents a movie with n features.
+% So the idea is to use the matrix R as a mask for the X*Theta' matrix, to
+% select only those elements where R == 1
 
+previsions = R .* (X * Theta');
 
+% Compute cost without regularization
+J = (1/2) * sum(sum((previsions - Y) .^ 2)) + ...
+    (lambda/2) * (sum(sum(Theta .^ 2)) + sum(sum(X .^ 2)));
 
-
-
-
-
-
-
-
-
-
-
-
+% Compute gradients to feed fmincg optimization package
+% Obs.: done purely using matrix size correspondence
+X_grad = ((previsions - Y) * Theta) + lambda * X;
+Theta_grad = ((previsions - Y)' * X) + lambda * Theta;
 
 % =============================================================
 
+% Unroll 
 grad = [X_grad(:); Theta_grad(:)];
 
 end
